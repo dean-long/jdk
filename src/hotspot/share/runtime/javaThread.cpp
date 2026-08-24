9ed046ba69946ea23ca48734e2546bea8636d8e0
Dean Long <17332032+dean-long@users.noreply.github.com>
2026-08-24 23:27:12 +0000

    better leaf call detection

--- src/hotspot/share/runtime/javaThread.cpp
+++ src/hotspot/share/runtime/javaThread.cpp
@@ -290,8 +290,20 @@ void JavaThread::check_for_valid_safepoint_state() {
   // are held.
   check_possible_safepoint();
 
-  if (thread_state() != _thread_in_vm) {
-    fatal("LEAF method calling lock?");
+  switch (thread_state()) {
+  case _thread_in_vm:
+  case _thread_in_Java:
+    // In debug builds, leaf entries use NoHandleMark and NoSafepointVerifier,
+    // while non-leaf entries use HandleMarkCleaner.
+    if (last_handle_mark() == nullptr) {
+      fatal("LEAF method calling lock?");
+    }
+    if (handle_area()->no_handle_mark_active()) {
+      fatal("LEAF method calling lock?");
+    }
+    break;
+  default:
+    fatal("illegal thread state %d, LEAF method calling lock?", thread_state());
   }
 
   if (GCALotAtAllSafepoints) {
